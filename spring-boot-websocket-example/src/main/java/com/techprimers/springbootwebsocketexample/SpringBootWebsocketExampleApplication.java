31e8910a31b497b9b199bdb4b9c5c9e8bd04d243
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2019-12-02 22:28:30 +0530

    create web app without saving data in database

---
+++ spring-boot-websocket-example/src/main/java/com/techprimers/springbootwebsocketexample/SpringBootWebsocketExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techprimers.springbootwebsocketexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebsocketExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebsocketExampleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-websocket-example/src/test/java/com/techprimers/springbootwebsocketexample/SpringBootWebsocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprimers.springbootwebsocketexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebsocketExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
